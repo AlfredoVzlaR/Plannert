5a4d455c603941520a4beb602bb497f8d6468e6f
ElisaVelasquez <101837234+ElisaVelasquez@users.noreply.github.com>
2023-03-30 22:16:20 -0700

    ajustes

--- Moviles/app/src/main/java/mx/edu/plannert/Bienvenida2.kt
+++ Moviles/app/src/main/java/mx/edu/plannert/Bienvenida2.kt
@@ -1,11 +1,25 @@
 package mx.edu.plannert
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Bienvenida2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bienvenida2)
+        var boton: Button =findViewById(R.id.btnCrearLista)
+        val later : TextView = findViewById(R.id.tvMasTarde)
+
+        boton.setOnClickListener{
+            val intent = Intent(this,Registro::class.java)
+            startActivity(intent)
+        }
+        later.setOnClickListener{
+            val intent = Intent(this,Registro::class.java)
+            startActivity(intent)
+        }
     }
 }
