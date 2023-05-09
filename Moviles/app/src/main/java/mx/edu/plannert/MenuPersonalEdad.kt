978e362ea079369314ce0594c53d2b9e0055def3
PaulinaCtz <paulina.cortez228557@potros.itson.edu.mx>
2023-05-09 15:17:42 -0700

    Modificar edad en firebase

--- Moviles/app/src/main/java/mx/edu/plannert/MenuPersonalEdad.kt
+++ Moviles/app/src/main/java/mx/edu/plannert/MenuPersonalEdad.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,9 +24,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MenuPersonalEdad : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    // Declarar una variable para almacenar la fecha seleccionada
+    private var fechaSeleccionada: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +42,104 @@ class MenuPersonalEdad : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_personal_edad, container, false)
+        val view = inflater.inflate(R.layout.fragment_menu_personal_edad, container, false)
+
+        val btnCambiarFecha = view.findViewById<Button>(R.id.buttonCambiarEdad)
+        val calendarView = view.findViewById<CalendarView>(R.id.calendarView)
+
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            // Almacenar la fecha seleccionada en una variable
+            val calendar = Calendar.getInstance()
+            calendar.set(year, month, dayOfMonth)
+            fechaSeleccionada = calendar.timeInMillis
+        }
+
+        btnCambiarFecha.setOnClickListener {
+            // Verificar que se haya seleccionado una fecha
+            if (fechaSeleccionada == 0L) {
+                Toast.makeText(
+                    requireContext(),
+                    "¡Debes seleccionar una fecha de nacimiento!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            // Obtener la referencia a la base de datos
+            val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+            val usuariosRef: DatabaseReference = database.getReference("usuarios")
+            val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+            // Obtener el usuario actual
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                val email = currentUser.email
+
+                usuariosRef.orderByChild("email").equalTo(email)
+                    .addListenerForSingleValueEvent(object :
+                        ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            for (usuarioSnapshot in dataSnapshot.children) {
+                                val usuarioKey = usuarioSnapshot.key
+                                // Actualizar la fecha de nacimiento en la base de datos
+                                usuariosRef.child(usuarioKey!!).child("fechaNacimiento")
+                                    .setValue(getFormattedDate())
+                                    .addOnCompleteListener { task ->
+                                        if (task.isSuccessful) {
+                                            // La fecha de nacimiento se actualizó correctamente
+                                            Toast.makeText(
+                                                requireContext(),
+                                                "¡Fecha de nacimiento modificada!",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        } else {
+                                            // Manejar el caso de error al actualizar la fecha de nacimiento
+                                            Toast.makeText(
+                                                requireContext(),
+                                                "¡Ha ocurrido un error!",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                    }
+                                return
+                            }
+
+                            // Manejar el caso de email no encontrado en la base de datos
+                            Toast.makeText(
+                                requireContext(),
+                                "¡El email actual que has ingresado es incorrecto!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {
+                            // Manejar el caso de error en la consulta de la base de datos
+                            Toast.makeText(
+                                requireContext(),
+                                "¡Ha ocurrido un error en la consulta de la base de datos!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    })
+            }
+        }
+
+        return view
+    }
+
+    // Método para obtener la fecha formateada en "dd/MM/yyyy"
+    private fun getFormattedDate(): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = fechaSeleccionada
+
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        return dateFormat.format(calendar.time)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuPersonalEdad.
-         */
-        // TODO: Rename and change types and number of parameters
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             MenuPersonalEdad().apply {
