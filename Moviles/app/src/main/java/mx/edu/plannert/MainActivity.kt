a1ef82cb1cd332bd7198b7ae4e03153ff5cefb19
PaulinaCtz <paulina.cortez228557@potros.itson.edu.mx>
2023-05-08 21:39:20 -0700

    Registro e inicio de sesión con firebase

--- Moviles/app/src/main/java/mx/edu/plannert/MainActivity.kt
+++ Moviles/app/src/main/java/mx/edu/plannert/MainActivity.kt
@@ -4,30 +4,55 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Plannert)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        auth = FirebaseAuth.getInstance()
+
         val registro: TextView = findViewById(R.id.tv_registrate)
-        var botonIS:Button=findViewById(R.id.btnIniciarSesion)
-        val necesitoAyuda : TextView = findViewById(R.id.necesitoAyuda)
+        val botonIS: Button = findViewById(R.id.btnIniciarSesion)
+        val necesitoAyuda: TextView = findViewById(R.id.necesitoAyuda)
 
-        registro.setOnClickListener{
-            val intent = Intent(this,Registro::class.java)
+        registro.setOnClickListener {
+            val intent = Intent(this, Registro::class.java)
             startActivity(intent)
         }
 
-        botonIS.setOnClickListener{
-            val intent = Intent(this,Introductorio::class.java)
-            startActivity(intent)
+        botonIS.setOnClickListener {
+            val email_txt: EditText = findViewById(R.id.txt_emailInicio)
+            val contraseña_txt: EditText = findViewById(R.id.txt_contraseñaInicio)
+
+            val email = email_txt.text.toString()
+            val contraseña = contraseña_txt.text.toString()
+
+            // Iniciar sesión con el usuario y la contraseña
+            auth.signInWithEmailAndPassword(email, contraseña)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // El inicio de sesión fue exitoso, redirigir a la página Introductorio
+                        val intent = Intent(this, Introductorio::class.java)
+                        startActivity(intent)
+                        finish() // Evitar que el usuario regrese a la pantalla de inicio de sesión
+                    } else {
+                        // El inicio de sesión falló, mostrar mensaje de error
+                        Toast.makeText(this, "Inicio de sesión fallido", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
 
-        necesitoAyuda.setOnClickListener{
-            val intent = Intent(this,help::class.java)
+        necesitoAyuda.setOnClickListener {
+            val intent = Intent(this, help::class.java)
             startActivity(intent)
         }
     }
